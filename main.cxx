3ff7c28e3d3a78852ecaf73b40cc87c8c35e257f
Nathanael Farley <prue-ubuntu>
2013-07-15 00:42:36 +0100

    second commit

--- main.cxx
+++ main.cxx
@@ -1,13 +1,12 @@
-#include <gtkmm.h>
+#include "window.h"
+#include <gtkmm/application.h>
 
 int main(int argc, char *argv[])
 {
-  Glib::RefPtr<Gtk::Application> app =
-    Gtk::Application::create(argc, argv,
-      "org.gtkmm.examples.base");
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-  Gtk::Window window;
-  window.set_default_size(200, 200);
+  Window window;
 
+  // Shows the window and returns when it is closed.
   return app->run(window);
 }
