26a61f9d8acae651ceeb12fcfd1df2a8a4327db4
Nathanael Farley <prue-ubuntu>
2013-07-15 09:09:30 +0100

    getting the menu working

--- window.h
+++ window.h
@@ -10,6 +10,15 @@ public:
   virtual ~Window();
 
 private:
+  //Signal handlers:
+  void on_menu_file_new_generic();
+  void on_menu_file_quit();
+  void on_menu_others();
+
+  void on_menu_choices_one();
+  void on_menu_choices_two();
+
+
   // Signal handlers:
   void on_button_quit();
   void on_button_numbered(const Glib::ustring& data);
@@ -17,6 +26,14 @@ private:
   // Child widgets:
   Gtk::Grid m_grid;
   Gtk::Button m_skip_to_start, m_rewind, m_play, m_pause, m_stop, m_fast_forward, m_skip_to_end, m_button_quit;
+
+  //Child widgets for menu:
+  Gtk::Box m_Box;
+
+  Glib::RefPtr<Gtk::UIManager> m_refUIManager;
+  Glib::RefPtr<Gtk::ActionGroup> m_refActionGroup;
+  Glib::RefPtr<Gtk::RadioAction> m_refChoiceOne, m_refChoiceTwo;
+
 };
 
 #endif /* GTKMM_WINDOW_H */
