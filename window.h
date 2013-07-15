58ad86bab3a4299593883c25113d2ab88cb6839b
Nathanael Farley <prue-ubuntu>
2013-07-15 20:01:42 +0100

    added more functions

--- window.h
+++ window.h
@@ -7,30 +7,34 @@
 class Window : public Gtk::Window
 {
 public:
-  Window();
-  virtual ~Window();
+Window();
+virtual ~Window();
 
 protected:
-  //Signal handlers:
-  void on_menu_file_new_generic();
-  void on_menu_file_quit();
-  void on_menu_file_open();
-  void on_menu_others();
+//Signal handlers:
+void on_menu_file_new_generic();
+void on_menu_file_quit();
+void on_menu_file_open();
+void on_menu_play();
+void on_menu_pause();
+void on_menu_stop();
+void on_menu_forward();
+void on_menu_others();
 
-  void on_menu_choices_one();
-  void on_menu_choices_two();
+void on_menu_choices_one();
+void on_menu_choices_two();
 
-  //Child widgets:
-  Gtk::Box m_Box;
+//Child widgets:
+Gtk::Box m_Box;
 
-  Glib::RefPtr<Gtk::UIManager> m_refUIManager;
-  Glib::RefPtr<Gtk::ActionGroup> m_refActionGroup;
-  Glib::RefPtr<Gtk::RadioAction> m_refChoiceOne, m_refChoiceTwo;
+Glib::RefPtr<Gtk::UIManager> m_refUIManager;
+Glib::RefPtr<Gtk::ActionGroup> m_refActionGroup;
+Glib::RefPtr<Gtk::RadioAction> m_refChoiceOne, m_refChoiceTwo;
 
 
-  libvlc_instance_t *inst;
-  libvlc_media_player_t *mp;
-  libvlc_media_t *m;
+libvlc_instance_t *inst;
+libvlc_media_player_t *mp;
+libvlc_media_t *m;
 
 };
 
