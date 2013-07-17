64a63f5b2eac0d9a6bdb3c0ae505affbfdc3c0f6
Nathanael Farley <prue-ubuntu>
2013-07-17 09:40:19 +0100

    Added pedal.h

--- main.cxx
+++ main.cxx
@@ -1,12 +1,18 @@
 #include "window.h"
+#include "pedal.h"
 #include <gtkmm/application.h>
+
+#include <vlc/vlc.h>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create
+    (argc, argv, "org.gtkmm.example");
 
   Window window;
 
   // Shows the window and returns when it is closed.
   return app->run(window);
+
 }
