12eeee140c7f391d2690e889d08fa7005433f285
Nathanael Farley <prue-ubuntu>
2013-07-18 20:48:28 +0100

    added multithread example

--- pedal.cxx
+++ pedal.cxx
@@ -1,46 +1,40 @@
 #include "pedal.h"
 #include <gtkmm.h>
-#include <iostream
+#include <iostream>
 
-class Pedal {
-  public:
+Pedal::Pedal() : thread(0), stop(false) {
+  std::cout << "I made a Pedal" << std::endl;
+}
  
-    Pedal() : thread(0), stop(false) {}
+// Called to start the processing on the thread
+void Pedal::start () {
+  std::cout << "Pedal started" << std::endl;
+  thread = Glib::Thread::create(sigc::mem_fun(*this, &Pedal::run), true);
+  std::cout << "Pedal start ended" << std::endl;
+}
  
-    // Called to start the processing on the thread
-    void start () {
-      thread = Glib::Thread::create(sigc::mem_fun(*this, &Pedal::run), true);
+// When shutting down, we need to stop the thread
+Pedal::~Pedal() {
+  {
+    Glib::Mutex::Lock lock (mutex);
+    stop = true;
+  }
+  if (thread)
+    thread->join(); // Here we block to truly wait for the thread to complete
+}
+ 
+// This is where the real work happens
+void Pedal::run () {
+    std::cout << "Thread write!" << std::endl;
+  while(true) {
+    {
+      Glib::Mutex::Lock lock (mutex);
+      if (stop) break;
     }
- 
-    // When shutting down, we need to stop the thread
-    ~Pedal() {
-      {
-        Glib::Mutex::Lock lock (mutex);
-        stop = true;
-      }
-      if (thread)
-        thread->join(); // Here we block to truly wait for the thread to complete
-    }
- 
-    Glib::Dispatcher sig_done;
- 
-  protected:
-    // This is where the real work happens
-    void run () {
- 
-      while(true) {
-        {
-          Glib::Mutex::Lock lock (mutex);
-          if (stop) break;
-        }
-        sleep(5);
-        std::cout << "Thread write!" << std::endl;
-        sig_done();
-        break;
-      }
-    }
- 
-    Glib::Thread * thread;
-    Glib::Mutex mutex;
-    bool stop;
-};
+    sleep(5);
+    std::cout << "Thread write!" << std::endl;
+    sig_done();
+    break;
+  }
+}
+
