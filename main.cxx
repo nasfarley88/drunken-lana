d6d6d83329c310713942d00161c8c7efba5ec025
Nathanael Farley <prue-ubuntu>
2013-07-18 19:31:53 +0100

    Preparing for multi-threading for pedal use

--- main.cxx
+++ main.cxx
@@ -1,16 +1,21 @@
 #include "window.h"
-#include "pedal.h"
 #include <gtkmm/application.h>
-
+#include "hidapi.h"
+#include <iostream>
 #include <vlc/vlc.h>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+
+  // Gheck if we're ready to support threads, and if not, make it ready.
+  if(!Glib::thread_supported()) Glib::thread_init();
+  
   Glib::RefPtr<Gtk::Application> app = Gtk::Application::create
     (argc, argv, "org.gtkmm.example");
 
   Window window;
+
 
   // Shows the window and returns when it is closed.
   return app->run(window);
