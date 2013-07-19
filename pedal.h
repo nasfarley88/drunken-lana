8b4db6f2997d532c934f8b7bf81dc96303354e7b
Nathanael Farley <prue-ubuntu>
2013-07-19 09:05:25 +0100

    Basic pedal working

--- pedal.h
+++ pedal.h
@@ -3,14 +3,22 @@
 #include <gtkmm.h>
 #include "hidapi.h"
 
-class Pedal {
+class Pedal : public sigc::trackable
+{
  public:
-  Pedal();
+  Pedal(void *);
   ~Pedal();
   void start();
   Glib::Dispatcher sig_done;
   
  protected:
+  hid_device *pedal;
+  unsigned char pedal_buf[256];
+  bool read_pedal(int x);
+  const int read_pedal_timeout;
+  void * mywindow;
+
+
   void run();
   
   // Multithread stuff
